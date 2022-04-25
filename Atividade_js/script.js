972d7c3adfbd93fef42d1ec91a8895be649bf4de
CharlieCernov <100284435+CharlieCernov@users.noreply.github.com>
2022-04-25 11:59:28 -0300

    Add files via upload

---
+++ Atividade_js/script.js
@@ -0,0 +1,10 @@
+function clicou(){
+    document.querySelector(".quad").innerHTML = "Clicou"
+}
+function passouoMouse(){
+    document.querySelector(".quad").innerHTML = "Passou o Mouse"
+}
+
+function tirouomouse(){
+    document.querySelector(".quad").innerHTML = "Tirou o Mouse"
+}
